3a1c3779b9288724f29a7eda7e5ad6aafba867a4
Behnam <Behnam.khorshidian@gmail.com>
2021-01-24 09:32:46 +0330

    Stable V.1.0.0

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,42 @@
-// Sticky Navigation 
+// Sticky Navigation  and Animation
 
-var sectionFeatures = document.querySelector(".section-features");
 var nav = document.querySelector("nav");
+var sectionFeatures = document.querySelector(".section-features");
+var sectionFeaturesColumn = document.querySelectorAll(".section-features .row .col");
+var sectionSteps = document.querySelector(".section-steps");
+var sectionLibraries = document.querySelector(".section-libraries");
+var sectionLibrariesColumn = document.querySelectorAll(".section-libraries .row .col");
+var sectionPricing = document.querySelector(".section-pricing");
 
 window.onscroll = function() {
+
+    // Sticky Nav and features Animation
     if (sectionFeatures.getBoundingClientRect().top < 100) {
         nav.classList.add("sticky-nav");
+        
+        for (var i = 0; i < sectionFeaturesColumn.length ; i++) {
+            sectionFeaturesColumn[i].classList.add("js--opacity");
+        }
+
     } else if (sectionFeatures.getBoundingClientRect().top > 100) {
         nav.classList.remove("sticky-nav");
+    }
+
+    // App screen animation
+    if (sectionSteps.getBoundingClientRect().top < 50) {
+        document.querySelector(".app-screen").classList.add("animate__animated" ,  "animate__fadeInUp");
+    }
+
+    // Libraries section animation
+    if (sectionLibraries.getBoundingClientRect().top < 50) {
+        for (var i = 0; i < sectionLibrariesColumn.length ; i++) {
+            sectionLibrariesColumn[i].classList.add("js--opacity");
+        }
+    }
+
+    // Pricing section animation
+    if (sectionPricing.getBoundingClientRect().top < 50) {
+        document.querySelectorAll(".pricing-box")[0].classList.add("js--pulse-animation");
     }
 }
 
